1ba6f23af3276a025ae9b45c28d76178397b01dc
Nic <nicolacognata@gmail.com>
2019-07-30 21:44:11 -0400

    Restart project

---
+++ Price Point/PricePointHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Price_Point
+{
+	public class PricePointHub : Hub
+	{
+		public void Hello() {
+			Clients.All.hello();
+		}
+	}
+}
