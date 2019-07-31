faa8fabfde1df9508a5b9746101b3327dde0fabe
Nic <nicolacognata@gmail.com>
2019-07-30 22:44:18 -0400

    Start building framework

---
+++ Price Point/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Price_Point.Startup))]
+
+namespace Price_Point
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app) {
+			app.MapSignalR();
+		}
+	}
+}
